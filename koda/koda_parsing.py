cd10d633cbb5db38dbbcbf8c9212826c09489f01
Jonas <jonas.mueller.contact@gmail.com>
2024-12-11 18:15:38 +0100

    Incorporate pykoda parsing logic

--- koda/koda_parsing.py
+++ koda/koda_parsing.py
@@ -1,6 +1,9 @@
+import contextlib
 import json
+import warnings
 import zipfile
 
+import numpy as np
 import py7zr
 import os
 import pandas as pd
@@ -48,6 +51,105 @@ def unzip_data(input_path: str, data_dir = DATA_DIR):
     return output_path
 
 
+def _is_json(series: pd.Series) -> bool:
+    try:
+        return isinstance(series[0][0], dict)
+    except TypeError:
+        return False
+
+
+def unpack_jsons(df: pd.DataFrame) -> pd.DataFrame:
+    keys_to_sanitise = []
+    for k in list(df.keys()):
+        # If the content is a json, unpack and remove
+        if df[k].dtype == np.dtype('O') and _is_json(df[k]):
+            keys_to_sanitise.append(k)
+
+    if keys_to_sanitise:
+        indexes = []
+        unpacked = {k: [] for k in keys_to_sanitise}
+        for ix in df.index:
+            for k in keys_to_sanitise:
+                this_unpack = pd.json_normalize(df[k][ix])
+                unpacked[k].append(this_unpack)
+                indexes.extend(ix for _ in range(len(this_unpack)))
+
+        df.drop(keys_to_sanitise, axis='columns', inplace=True)
+
+        unpacked_series = []
+        for k in keys_to_sanitise:
+            this_df = pd.concat(unpacked[k], axis='index').reset_index(drop=True)
+            this_df.rename(columns={curr_name: '_'.join((k, curr_name)) for curr_name in this_df.keys()},
+                           inplace=True)
+            unpacked_series.append(this_df)
+
+        repeated = df.iloc[indexes].reset_index(drop=True)
+        df = pd.concat([repeated] + unpacked_series, axis='columns')
+
+    for k in df.keys():
+        if df[k].dtype == np.dtype('O') and _is_json(df[k]):
+            warnings.warn(RuntimeWarning(f'There are extra json in column {k}'))
+    return df
+
+
+def normalize_keys(df: pd.DataFrame) -> None:
+    """Reformat the name of the keys to a consistent format, according to GTFS"""
+    renames = {'tripUpdate_trip_tripId': 'trip_id', 'tripUpdate_trip_startDate': 'start_date',
+               'tripUpdate_trip_directionId': 'direction_id', 'tripUpdate_trip_routeId': 'route_id',
+               'tripUpdate_trip_scheduleRelationship': 'schedule_relationship',
+               'tripUpdate_trip_startTime': 'start_time',
+               'tripUpdate_timestamp': 'timestamp', 'tripUpdate_vehicle_id': 'vehicle_id',
+               'stopSequence': 'stop_sequence', 'stopId': 'stop_id',
+               'scheduleRelationship': 'schedule_relationship2',
+               'vehicle_trip_tripId': 'trip_id', 'vehicle_trip_scheduleRelationship': 'schedule_relationship',
+               'vehicle_timestamp': 'timestamp', 'vehicle_vehicle_id': 'vehicle_id',
+               'vehicle_trip_startTime': 'start_time', 'vehicle_trip_startDate': 'start_date',
+               'vehicle_trip_routeId': 'route_id', 'vehicle_trip_directionId': 'direction_id',
+               'tripUpdate_stopTimeUpdate_stopSequence': 'stop_sequence',
+               'tripUpdate_stopTimeUpdate_stopId': 'stop_id',
+               'tripUpdate_stopTimeUpdate_arrival_delay': 'arrival_delay',
+               'tripUpdate_stopTimeUpdate_arrival_time': 'arrival_time',
+               'tripUpdate_stopTimeUpdate_departure_delay': 'departure_delay',
+               'tripUpdate_stopTimeUpdate_departure_time': 'departure_time',
+               'tripUpdate_stopTimeUpdate_arrival_uncertainty': 'arrival_uncertainty',
+               'tripUpdate_stopTimeUpdate_departure_uncertainty': 'departure_uncertainty',
+               'alert_activePeriod_start': 'period_start', 'alert_activePeriod_end': 'period_end',
+               'alert_informedEntity_routeId': 'route_id', 'alert_informedEntity_stopId': 'stop_id',
+               'alert_informedEntity_trip_tripId': 'trip_id',
+               'alert_informedEntity_trip_scheduleRelationship': 'schedule_relationship',
+               'alert_headerText_translation_text': 'header_text',
+               'alert_descriptionText_translation_text': 'description_text',
+               }
+    df.rename(columns=renames, inplace=True)
+
+
+def sanitise_array(df: pd.DataFrame) -> None:
+    normalize_keys(df)
+
+    # Remove columns and rows with all NaNs
+    df.dropna(axis=0, how='all', inplace=True)
+    df.dropna(axis=1, how='all', inplace=True)
+
+    # Remove old indexes
+    df.drop(columns='level_0', inplace=True, errors='ignore')
+
+    # Remove duplicated entries, ignoring timpestamps and index
+    keys = list(df.keys())
+    with contextlib.suppress(ValueError):
+        keys.remove('timestamp')
+        keys.remove('index')
+
+        # These may be updated in the database, so ignore as well
+        keys.remove('arrival_delay')
+        keys.remove('arrival_time')
+        keys.remove('departure_delay')
+        keys.remove('departure_time')
+        keys.remove('arrival_uncertainty')
+        keys.remove('departure_uncertainty')
+
+    df.drop_duplicates(subset=keys, inplace=True, keep='last')
+
+# For context, see _pasrse_gtfs in pykoda project
 def read_pb_to_dataframe(input_path:str, operator, feed_type, year, month, day, hour, minute, second):
     filename = f"{operator}-{feed_type.lower()}-{year}-{month}-{day}T{hour}-{minute}-{second}Z.pb"
     file_path = os.path.join(input_path, operator, feed_type, year, month, day, hour, filename)
@@ -62,5 +164,41 @@ def read_pb_to_dataframe(input_path:str, operator, feed_type, year, month, day, 
     msg_dict = json.loads(msg_json)
 
     df = pd.json_normalize(msg_dict.get('entity', dict()), sep='_')
-
+    df = unpack_jsons(df)
+    df.reset_index(drop=True, inplace=True)
     return df
+
+
+# For context, see merge_files in pykoda project
+def read_rt_folder_to_df(input_path:str, operator, feed_type, year, month, day, hour):
+    df_list = []
+    for minute in range(60):
+        for second in range(60):
+            try:
+                df = read_pb_to_dataframe(input_path, operator, feed_type, year, month, day, hour, str(minute), str(second))
+                df_list.append(df)
+            except FileNotFoundError:
+                pass
+    merged_df = pd.concat(df_list, axis=0)
+    # Force casts:
+    castings = dict()
+    for k in merged_df.keys():
+        if 'timestamp' in k:  # Timestamps should be ints, not strings
+            castings[k] = np.int64
+        elif k == 'id':
+            castings[k] = np.int64
+    merged_df.dropna(how='all', inplace=True)  # Remove rows of only NaNs
+    merged_df = merged_df.astype(castings)
+
+    # Remove dots from column names
+    rename = dict((k, k.replace('.', '_')) for k in merged_df.keys() if '.' in k)
+    merged_df.rename(columns=rename, inplace=True)
+
+    # Clean up duplicates, fix keys, etc
+    sanitise_array(merged_df)
+
+    if merged_df.empty:  # Feather does not support a DF without columns, so add a dummy one
+        merged_df['_'] = np.zeros(len(merged_df), dtype=np.bool_)
+
+    merged_df.reset_index(inplace=True)
+    return merged_df
