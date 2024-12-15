b2f45786d6020fae82ca33b50551620ea72d6ffe
Jonas <jonas.mueller.contact@gmail.com>
2024-12-15 16:23:00 +0100

    Add initial HW feature group code.

--- koda_feature_pipeline.py
+++ koda_feature_pipeline.py
@@ -1,3 +1,7 @@
+import os
+import sys
+
+import hopsworks
 import pandas as pd
 
 from koda.koda_constants import OperatorsWithRT
@@ -6,6 +10,8 @@ import koda.koda_transform as kt
 
 ON_TIME_MIN_SECONDS = -180
 ON_TIME_MAX_SECONDS = 300
+
+SAVE_TO_HW = False
 
 if __name__ == "__main__":
     DATE = "2023-02-06"
@@ -70,4 +76,31 @@ if __name__ == "__main__":
 
     # TODO: Change based on model?
     final_metrics.fillna(0, inplace=True) # Fill NaNs with 0 - During night time (00:00-02:00), no data is generally available
-    final_metrics.to_csv("final_metrics.csv", index=False)
+
+    # TODO: Data expectations?
+
+    if not SAVE_TO_HW:
+        final_metrics.to_csv("final_metrics.csv", index=False)
+        sys.exit(0)
+
+    if os.environ.get("HOPSWORKS_API_KEY") is None:
+        os.environ["HOPSWORKS_API_KEY"] = open(".hw_key").read()
+
+    project = hopsworks.login()
+    fs = project.get_feature_store()
+
+    delays_fg = fs.get_or_create_feature_group(
+        name='delays',
+        description='Aggregated delay metrics per hour per day',
+        version=1,
+        primary_key=['arrival_time_bin'],
+        event_time='arrival_time_bin'
+    )
+    delays_fg.insert(final_metrics)
+    delays_fg.update_feature_description("arrival_time_bin", "Hourly time bin by stop arrival time")
+    delays_fg.update_feature_description("mean_arrival_delay_seconds", "Mean stop arrival delay in seconds")
+    delays_fg.update_feature_description("max_arrival_delay_seconds", "Max stop arrival delay in seconds")
+    delays_fg.update_feature_description("mean_departure_delay_seconds", "Mean stop departure delay in seconds")
+    delays_fg.update_feature_description("max_departure_delay_seconds", "Max stop departure delay in seconds")
+    delays_fg.update_feature_description("on_time_mean_percent", "Percentage of stops on time (-2 to 5 minutes)")
+    delays_fg.update_feature_description("stop_count", "Number of stops in the hour")
