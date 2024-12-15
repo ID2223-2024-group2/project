f612221f23be731c12eca38900ad1c153e674530
Jonas <jonas.mueller.contact@gmail.com>
2024-12-15 17:04:21 +0100

    Add another missing dependency. Allow nr of processes to be defined by env.

--- koda/koda_pipeline.py
+++ koda/koda_pipeline.py
@@ -9,6 +9,10 @@ import koda.koda_parse as kp
 from koda.koda_constants import FeedType, OperatorsWithRT
 
 
+USE_PROCESSES = os.environ.get("USE_PROCESSES")
+if USE_PROCESSES is None:
+    USE_PROCESSES = os.cpu_count() - 2
+
 def get_trip_updates_for_day(date: str, operator: OperatorsWithRT) -> pd.DataFrame:
     rt_folder_path = kp.get_rt_dir_path(operator.value, date)
     if os.path.exists(f"{rt_folder_path}/.feathers_complete"):
@@ -22,7 +26,7 @@ def get_trip_updates_for_day(date: str, operator: OperatorsWithRT) -> pd.DataFra
     folder_path = kp.unzip_gtfs_archive(archive_path, remove_archive_after=True)
     print(f"Unzipped realtime data to {folder_path}")
 
-    with ProcessPoolExecutor(max_workers=os.cpu_count() - 2) as executor:
+    with ProcessPoolExecutor(max_workers=USE_PROCESSES) as executor:
         df = kt.read_rt_day_to_df(operator, FeedType.TRIP_UPDATES, date, remove_folder_after=True, executor=executor)
 
     with open(f"{rt_folder_path}/.feathers_complete", "w") as f:
