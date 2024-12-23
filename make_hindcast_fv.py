e7e739399aceebd49371b867d3fd3a41ffb17a96
Paul Hübner <phubner@kth.se>
2024-12-23 22:47:31 +0100

    Hotfixes and sync.

--- make_hindcast_fv.py
+++ make_hindcast_fv.py
@@ -16,17 +16,9 @@ if __name__ == "__main__":
         right_on=['date', 'transport_type'],
         join_type='inner'
     )
-    labels = ['mean_delay_change_seconds', 'max_delay_change_seconds', 'min_delay_change_seconds',
-                             'var_delay_change_seconds',
-                             'mean_arrival_delay_seconds', 'max_arrival_delay_seconds', 'min_arrival_delay_seconds',
-                             'var_arrival_delay',
-                             'mean_departure_delay_seconds', 'max_departure_delay_seconds',
-                             'min_departure_delay_seconds', 'var_departure_delay',
-                             'mean_on_time_percent', 'mean_final_stop_delay_seconds']
     feature_view = fs.get_or_create_feature_view(
         name='monitor_fv',
         description="monitoring predictions",
         version=FV_VERSION,
-        labels=labels,
         query=selected_features,
     )
